701f6408ff4a45ec5a0a802413b23619a1838461
Doyoung Han <rbfwmqwntm@naver.com>
2022-11-27 08:26:04 -0800

    Automated Luggage Transfer System Support v0.2.0.0 (221128-0125)
    - backclip implemented.
    
    end.

--- backclip/module/template/main.py
+++ backclip/module/template/main.py
@@ -13,12 +13,8 @@ from StringBuffer import StringBuffer
 #
 import base64
 
-
 #
-in_filename = 'main.py'
-fin = open(in_filename, 'r')
-in_lines = [_line.rstrip('\r\n') for _line in fin.readlines()]
-fin.close()
+from PIL import Image
 
 
 #
@@ -26,52 +22,11 @@ START_STRING = "HandyAutomatedLLSTransferSystemBackClipService..".encode('utf-8'
 
 
 #
-ALT_IMG_WIDTH = 800
-ALT_IMG_HEIGHT = 600
-ALT_CELL_LEN = 1
-
-
-
-#
-cell_width  = int(ALT_IMG_WIDTH / ALT_CELL_LEN)
-cell_height = int(ALT_IMG_HEIGHT / ALT_CELL_LEN)
-cell_len    = int(ALT_CELL_LEN)
-alt_cell_width  = -cell_width
-alt_cell_height = -cell_height
-alt_cell_len    = -cell_len
-bytes_cell_width  = alt_cell_width.to_bytes(2,  byteorder="little", signed=True)
-bytes_cell_height = alt_cell_height.to_bytes(2, byteorder="little", signed=True)
-bytes_cell_len    = alt_cell_len.to_bytes(2,    byteorder="little", signed=True)
-
-
-text = 'Lorem Ipsum'
-ba_text_utf8 = text.encode('utf-8')
-ba_text_base64 = base64.b64encode(ba_text_utf8)
-
-
-##
-#sb = StringBuffer(lines=in_lines)
-#
-#
-##
-#while sb.reading():
-#    line = sb.getline()
-#
-#    print(line)
-
-
-print(text)
-print(ba_text_utf8)
-print(ba_text_base64)
-
-
-#
-ba_encoded_text = START_STRING + bytes_cell_width + bytes_cell_height + bytes_cell_len + ba_text_base64
-print(ba_encoded_text)
-
-
-#
-from PIL import Image
+in_width = int(sys.argv[1])
+in_height = int(sys.argv[2])
+in_cell_len = int(sys.argv[3])
+in_filename = sys.argv[4]
+out_bmp_filename = sys.argv[5]
 
 
 #
@@ -82,26 +37,83 @@ def get_byte(ba_encoded_text, i: int):
 
 
 #
-out_bmp = Image.new('RGB', (800, 600))
+def run(ALT_IMG_WIDTH: int, ALT_IMG_HEIGHT: int, ALT_CELL_LEN: int, in_filename: str, out_bmp_filename: str):
+    cell_width  = int(ALT_IMG_WIDTH / ALT_CELL_LEN)
+    cell_height = int(ALT_IMG_HEIGHT / ALT_CELL_LEN)
+    cell_len    = int(ALT_CELL_LEN)
+    alt_cell_width  = -cell_width
+    alt_cell_height = -cell_height
+    alt_cell_len    = -cell_len
+    bytes_cell_width  = alt_cell_width.to_bytes(2,  byteorder="little", signed=True)
+    bytes_cell_height = alt_cell_height.to_bytes(2, byteorder="little", signed=True)
+    bytes_cell_len    = alt_cell_len.to_bytes(2,    byteorder="little", signed=True)
+
+    #
+    with open(in_filename, 'rb') as fin:
+        bin_data = fin.read()
+
+    ##
+    #text = in_text
+    #ba_text_utf8 = text.encode('utf-8')
+    #ba_text_base64 = base64.b64encode(ba_text_utf8)
+
+    #
+    ba_text_base64 = base64.b64encode(bin_data)
+    ba_encoded_text = START_STRING + bytes_cell_width + bytes_cell_height + bytes_cell_len + ba_text_base64
+
+    #
+    out_bmp = Image.new('RGB', (ALT_IMG_WIDTH, ALT_IMG_HEIGHT))
+
+    #
+    i = 0
+    for yi in range(cell_height):
+        for xi in range(cell_width):
+            byte_r = get_byte(ba_encoded_text, 3 * i)
+            byte_g = get_byte(ba_encoded_text, 3 * i + 1)
+            byte_b = get_byte(ba_encoded_text, 3 * i + 2)
+            #print('==========')
+            #print(f'(xi,yi)=({xi},{yi})')
+
+            # https://stackoverflow.com/questions/36468530/changing-pixel-color-value-in-pil
+            #out_bmp[xi, yi] = (byte_r, byte_g, byte_b)
+            #out_bmp.putpixel( (xi, yi), (byte_r, byte_g, byte_b) )
+            #print('--------')
+            for yi1 in range(cell_len):
+                for xi1 in range(cell_len):
+                    x = cell_len * xi + xi1
+                    y = cell_len * yi + yi1
+                    #print(f'(x,y)=({x},{y})')
+                    out_bmp.putpixel( (x, y), (byte_r, byte_g, byte_b) )
+            
+            i += 1
+
+    #
+    out_bmp.save(out_bmp_filename)
+    with open(out_bmp_filename + '.txt', 'w') as fout:
+        print(ba_text_base64.decode(), file=fout)
+
 
 #
-i = 0
-for yi in range(600):
-    for xi in range(800):
-        byte_r = get_byte(ba_encoded_text, 3 * i)
-        byte_g = get_byte(ba_encoded_text, 3 * i + 1)
-        byte_b = get_byte(ba_encoded_text, 3 * i + 2)
+run(
+    ALT_IMG_WIDTH=in_width,
+    ALT_IMG_HEIGHT=in_height,
+    ALT_CELL_LEN=in_cell_len,
+    in_filename=in_filename,
+    out_bmp_filename=out_bmp_filename,
+    )
 
-        #out_bmp[xi, yi] = (byte_r, byte_g, byte_b)
 
-        # https://stackoverflow.com/questions/36468530/changing-pixel-color-value-in-pil
-        out_bmp.putpixel( (xi, yi), (byte_r, byte_g, byte_b) )
-        
-        i += 1
-
+##
+#in_filename = 'main.py'
+#fin = open(in_filename, 'r')
+#in_lines = [_line.rstrip('\r\n') for _line in fin.readlines()]
+#fin.close()
+##
+#sb = StringBuffer(lines=in_lines)
 #
-#out_bmp.frombytes(ba_encoded_text)
-out_bmp.save('out.bmp')
-
-
-
+#
+##
+#while sb.reading():
+#    line = sb.getline()
+#
+#    print(line)
--- backclip/run.csh
+++ backclip/run.csh
@@ -1,2 +1,8 @@
 #!/bin/csh
+set exe_backclip = "python3 module/template/main.py"
 
+#
+echo "argv is [$argv]"
+
+#
+$exe_backclip $argv
